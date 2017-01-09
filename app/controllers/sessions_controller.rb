9246cb877284310af3d1f2c2909cc9c9ae8a48c4
nwielenga <nathan.wielenga@gmail.com>
2017-01-09 14:35:51 -0700

    not working right now, preserving state

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,21 +2,19 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-        #log in the user
         log_in user
+        remember user
         redirect_to user
     else
-        #throw error, login failed
-        flash.now[:danger] = "Invalid email/password combination"
+        flash.now[:danger] = 'Invalid email/password combination'
         render 'new'
     end
-  end
-
+end
   def destroy
-      log_out
+      log_out if logged_in?
       redirect_to root_url
   end
 end
